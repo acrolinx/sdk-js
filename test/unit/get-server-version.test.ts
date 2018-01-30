c781eef703438bb8a34d77aceb3a948ca79755db
marco <Marco.Stahl@acrolinx.com>
2018-01-30 16:13:37 +0100

    Removed unused test code

--- test/unit/get-server-version.test.ts
+++ test/unit/get-server-version.test.ts
@@ -2,17 +2,13 @@ import {AcrolinxEndpoint} from '../../src/index';
 import {
   DUMMY_SERVER_INFO,
   mockAcrolinxServer,
-  mockBrokenJsonServer,
   restoreOriginalFetch
 } from '../test-utils/mock-server';
 import {DUMMY_ENDPOINT_PROPS, DUMMY_SERVER_URL} from './common';
 
-const BROKEN_JSON_SERVER = 'http://broken-json-server';
-
 describe('getServerVersion', () => {
   beforeEach(() => {
     mockAcrolinxServer(DUMMY_SERVER_URL);
-    mockBrokenJsonServer(BROKEN_JSON_SERVER);
   });
 
   afterEach(() => {
