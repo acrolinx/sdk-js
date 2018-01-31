a27465f055ca705d1317a610d60d6f17487aef52
acromarco <Marco.Stahl@acrolinx.com>
2018-01-31 08:10:55 +0100

    Started with types for CheckResult

--- src/check.ts
+++ src/check.ts
@@ -1,4 +1,4 @@
-import {AudienceId, CheckType, ContentEncoding, ReportType, TermSetId} from './capabilities';
+import {AudienceId, CheckType, ContentEncoding, Goal, GoalId, ReportType, TermSetId} from './capabilities';
 import {URL} from './common-types';
 
 export interface CheckRange {
@@ -6,29 +6,35 @@ export interface CheckRange {
   end: number;
 }
 
+export type DocumentId = string;
+
 export interface CheckRequest {
   content: string;
   contentEncoding?: ContentEncoding;
-  checkOptions?: {
-    audienceId?: AudienceId;
-    termSetIds?: TermSetId[];
-    reportTypes?: ReportType[];
-    checkType?: CheckType;
-    partialCheckRanges?: CheckRange[];
-  };
+  checkOptions?: CheckOptions;
   batchId?: string;
-  document?: {
-    id?: string;
-    reference?: string;
-    author?: string;
-    mimeType?: string;
-    contentType?: string;
-    metaData?: MetaData;
-    displayInfo?: {
-      reference?: string;
-    }
-  };
+  document?: Document;
   // clientInfo ???
+}
+
+export interface CheckOptions {
+  audienceId?: AudienceId;
+  termSetIds?: TermSetId[];
+  reportTypes?: ReportType[];
+  checkType?: CheckType;
+  partialCheckRanges?: CheckRange[];
+}
+
+export interface Document {
+  id?: DocumentId;
+  reference?: string;
+  author?: string;
+  mimeType?: string;
+  contentType?: string;
+  metaData?: MetaData;
+  displayInfo?: {
+    reference?: string;
+  };
 }
 
 export interface MetaData {
@@ -43,14 +49,104 @@ export interface CheckResponse {
     status: URL;
     cancel: URL;
   };
+  // Lots of stuff that looks similar to CheckRequest
 }
-
 
 export interface CheckingStatus {
   id: CheckId;
+  documentId: DocumentId;
   state: 'done' | '???';
   percent: number;
+  message: string;
   links: {
     result: URL
   };
+}
+
+export interface CheckResult {
+  id: CheckId;
+  checkOptions: CheckOptions;
+  document: Document;
+  quality: {
+    score: number;
+    status: DocumentQualityStatus;
+  };
+  counts: {
+    sentences: number;
+    words: number;
+    issues: number;
+  };
+  goals: GoalWithIssueCount[];
+  issues: Issue[];
+}
+
+export interface Match {
+  extractedPart: string;
+  extractedBegin: number;
+  extractedEnd: number;
+  originalPart: string;
+  originalBegin: number;
+  originalEnd: number;
+}
+
+export interface Suggestion {
+  surface: string;
+  groupId: string;
+  replacements: { [matchIndex: string]: string }; // Objects would be easier to extend
+}
+
+export interface Action {
+  id: ActionId | string;
+  url: URL;
+  displayName: string;
+  icon: ActionIcon | string;
+  replacedBy: LinkId;
+}
+
+export type LinkId = string;
+
+export enum ActionId {
+  help = 'help',
+  termContributionForm = 'termContributionForm'
+}
+
+export enum ActionIcon {
+  help = 'help',
+  termContribute = 'term-contribute'
+}
+
+export interface Issue {
+  goalId: GoalId;
+  internalName: string; // Why?
+  displayName: string;
+  guidance: Html;
+  extractedSurface: string;
+  positionalInformation: {
+    hashes: {
+      issue: string;
+      environment: string;
+      index: string;
+    }
+  };
+  matches: Match[];
+  readonly: boolean;
+  issueLocations: {
+    locationId: string;
+    displayName: string;
+    values: { [id: string]: string };
+  };
+  suggestions: Suggestion[];
+  actions: Action[];
+  links: { [linkId: string]: URL };
+}
+
+
+export type Html = string;
+
+export interface GoalWithIssueCount extends Goal {
+  issueCount: number;
+}
+
+export enum DocumentQualityStatus {
+  red = 'red'
 }
