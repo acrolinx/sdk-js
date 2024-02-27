3afca1334270a652a4ffe6846e0720398a5e125d
Justin Horn <justin.horn@acrolinx.com>
2024-02-27 17:40:49 +0100

    DEV-37424; feat: add auth header

--- src/headers.ts
+++ src/headers.ts
@@ -19,3 +19,5 @@ export const HEADER_X_ACROLINX_AUTH = 'X-Acrolinx-Auth';
 export const HEADER_X_ACROLINX_BASE_URL = 'X-Acrolinx-Base-Url';
 export const HEADER_X_ACROLINX_CLIENT_LOCALE = 'X-Acrolinx-Client-Locale';
 export const HEADER_X_ACROLINX_APP_SIGNATURE = 'X-Acrolinx-App';
+
+export const HEADER_ACROLINX_ONE_AUTH = 'Authorization';
