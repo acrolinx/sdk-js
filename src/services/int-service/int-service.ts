185ecc93808e876faa78f3c0db732c51ff633e65
Paresh Deshmukh <paresh.deshmukh@acrolinx.com>
2024-04-30 01:45:46 +0200

    DEV-38253; feat: removed extra header from the request

--- src/services/int-service/int-service.ts
+++ src/services/int-service/int-service.ts
@@ -1,7 +1,23 @@
-import { AcrolinxEndpoint } from '../../index';
-import { IntegrationServiceConfigV1 } from './int-service.types';
+/*
+ * Copyright 2024-present Acrolinx GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-export const integrationServiceDefaultConfig: IntegrationServiceConfigV1 = {
+import { AcrolinxEndpoint } from '../../index';
+import { IntegrationServiceConfig } from './int-service.types';
+
+export const integrationServiceDefaultConfig: IntegrationServiceConfig = {
   activateGetSuggestionReplacement: false,
 };
 
@@ -9,17 +25,12 @@ export class IntService {
   private readonly intServiceBasePath = '/int-service/api/v1';
   constructor(
     private readonly endpoint: AcrolinxEndpoint,
-    private readonly _clientSignature?: string, // the sidebar might have not received a clientSignature
   ) {}
 
-  getConfig(accessToken: string): Promise<IntegrationServiceConfigV1> {
-    const headers = {
-      'X-Client-Signature': this._clientSignature || '',
-    };
-    return this.endpoint.getJsonFromPath<IntegrationServiceConfigV1>(
+  getConfig(accessToken: string): Promise<IntegrationServiceConfig> {
+    return this.endpoint.getJsonFromPath<IntegrationServiceConfig>(
       this.constructFullPath('/config'), 
       accessToken,
-      { headers },
     );
   }
 
--- test/unit/int-service.test.ts
+++ test/unit/int-service.test.ts
@@ -1,16 +1,11 @@
 import * as mockFetch from 'fetch-mock';
 import { AcrolinxEndpoint, IntService } from '../../src/index';
-import { DUMMY_ACCESS_TOKEN, DUMMY_CLIENT_SIGNATURE } from '../test-utils/mock-server';
+import { DUMMY_ACCESS_TOKEN } from '../test-utils/mock-server';
 import { DUMMY_ENDPOINT_PROPS } from './common';
 
 describe('Integration-service', () => {
   let endpoint: AcrolinxEndpoint;
   let intService: IntService;
-
-  beforeEach(() => {
-    endpoint = new AcrolinxEndpoint(DUMMY_ENDPOINT_PROPS);
-    intService = new IntService(endpoint, DUMMY_CLIENT_SIGNATURE);
-  });
 
   afterEach(() => {
     mockFetch.restore();
@@ -20,30 +15,41 @@ describe('Integration-service', () => {
     const configEndpointMatcher = 'end:/int-service/api/v1/config';
 
     it('truthy response for correct client signature', async () => {
+      endpoint = new AcrolinxEndpoint(DUMMY_ENDPOINT_PROPS);
+      intService = new IntService(endpoint);
       // Mock the endpoint with expected headers check and updated response property
       mockFetch.mock(configEndpointMatcher, (_url, opts) => {
         const headers = opts.headers as Record<string, string>; // Asserting headers to be of type Record<string, string>
-        if (headers['X-Client-Signature'] === DUMMY_CLIENT_SIGNATURE) {
+        if (headers['X-Acrolinx-Client'].includes(DUMMY_ENDPOINT_PROPS.client.signature)) {
           return {
             status: 200,
             body: { activateGetSuggestionReplacement: true }, // Updated property name
           };
         } else {
-          return { status: 401 }; // Unauthorized if the signature is wrong
+          return {
+            status: 200,
+            body: { activateGetSuggestionReplacement: false },
+          };
         }
       });
 
       const response = await intService.getConfig(DUMMY_ACCESS_TOKEN);
-      expect(response.activateGetSuggestionReplacement).toBe(true); // Assertion updated to check new property name
+      expect(response.activateGetSuggestionReplacement ).toBe(true); // Assertion updated to check new property name
     });
 
-    it('should fail without correct client signature', async () => {
-      // Test to ensure that the API responds correctly when the signature is missing or incorrect
-      mockFetch.mock(configEndpointMatcher, {
-        status: 401,
+    it('Default config response for unavailable client signature', async () => {
+      endpoint = new AcrolinxEndpoint(DUMMY_ENDPOINT_PROPS);
+      intService = new IntService(endpoint);
+      // Mock the endpoint with expected headers check and updated response property
+      mockFetch.mock(configEndpointMatcher, () => {
+          return {
+            status: 200,
+            body: { activateGetSuggestionReplacement: false },
+          };
       });
 
-      await expect(intService.getConfig(DUMMY_ACCESS_TOKEN)).rejects.toThrow();
+      const response = await intService.getConfig(DUMMY_ACCESS_TOKEN);
+      expect(response.activateGetSuggestionReplacement ).toBe(false); // Assertion updated to check new property name
     });
   });
 });
