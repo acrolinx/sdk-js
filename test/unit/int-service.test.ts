85b3a5d4a576f5be9d7ce54569e00444e242258d
Justin Horn <justin.horn@acrolinx.com>
2024-04-29 16:18:25 +0200

    DEV-38253; feat: add test

---
+++ test/unit/int-service.test.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024-present Acrolinx GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as mockFetch from 'fetch-mock';
+import { AcrolinxEndpoint, IntService } from '../../src/index';
+import { DUMMY_ACCESS_TOKEN } from '../test-utils/mock-server';
+import { DUMMY_ENDPOINT_PROPS } from './common';
+
+describe('Integration-service', () => {
+  let endpoint: AcrolinxEndpoint = new AcrolinxEndpoint(DUMMY_ENDPOINT_PROPS);
+  const intService = new IntService(endpoint);
+
+  afterEach(() => {
+    mockFetch.restore();
+  });
+
+  describe('/config', () => {
+    const aiEnabledMatcher = 'end:/int-service/api/v1/config';
+    it('truthy response', async () => {
+      mockFetch.mock(aiEnabledMatcher, {
+        status: 200,
+        body: { activateGAIGetSuggestionReplaceButton: true },
+      });
+
+      const response = await intService.getConfig(DUMMY_ACCESS_TOKEN);
+      expect(response.activateGAIGetSuggestionReplaceButton).toBe(true);
+    });
+  });
+});
