3a36d74fed16c7a589c6942c3792b11eb20d71b1
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2024-06-25 17:48:55 +0200

    fix: remove unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const sonarjs = require('eslint-plugin-sonarjs');
-const prettier = require('eslint-config-prettier');
 
 module.exports = tseslint.config({
   extends: [
@@ -27,8 +26,6 @@ module.exports = tseslint.config({
     '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/prefer-regexp-exec': 'off',
     '@typescript-eslint/no-empty-object-type': 'off',
-    //"import/no-cycle": "error",
     '@typescript-eslint/no-unsafe-argument': 'off',
-    //"prettier/prettier": "warn"
   },
 });
