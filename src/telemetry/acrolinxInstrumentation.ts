cead4920190b62dbace6d337ac8af7f7f48448ef
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2025-04-01 11:11:56 +0200

    fix: lint issues

--- src/telemetry/acrolinxInstrumentation.ts
+++ src/telemetry/acrolinxInstrumentation.ts
@@ -3,21 +3,18 @@ import { Counters, createDefaultCounters, setupMetrics } from './metrics/metrics
 import { MeterProvider } from '@opentelemetry/sdk-metrics';
 import { Logger } from '@opentelemetry/api-logs';
 import { AccessToken } from 'src/common-types';
-import { IntService } from 'src/services/int-service/int-service';
 import { AcrolinxEndpoint } from 'src';
 
 export class AcrolinxInstrumentation {
   private static acrolinxInstrumentation: AcrolinxInstrumentation;
-  private integrationService: IntService;
   public instruments: Instruments | undefined = undefined;
   private lastAccessChecked: number = 0;
   private intervalInMs = 60000; // 1 minute = 1 * 60 * 1000
   private isTelemetryAllowed = false;
   private config: TelemetryConfig;
 
-  private constructor(endpoint: AcrolinxEndpoint, config: TelemetryConfig) {
+  private constructor(_endpoint: AcrolinxEndpoint, config: TelemetryConfig) {
     this.config = config;
-    this.integrationService = new IntService(endpoint);
   }
 
   public static getInstance(endpoint: AcrolinxEndpoint, config: TelemetryConfig): AcrolinxInstrumentation {
